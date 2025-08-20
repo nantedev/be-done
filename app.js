3ff7b99a07f1ac56a39e816a88b45bdb0a375dd9
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-20 11:33:58 +0400

    🗃️ setup back end

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Don from "./models/don.js";
 
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173"
+};
+
+
+mongoose.connect("mongodb://localhost:27017/done");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connected successfully");
+});
+
+app.use(cors(corsOptions));
+
+app.get("/dons", async (req, res) => {
+  const dons = await Don.find({});
+  res.send(dons);
+});
 
 app.get("/", (req, res) => {
   res.send("express is running");
