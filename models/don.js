d5c6f206f3fbf6d1990628a673328b8d3027ab27
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-30 15:46:07 +0400

    🗃️  Seeds with images

--- models/don.js
+++ models/don.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const donSchema = new Schema({
   title: String,
-  price: String,
   description: String,
-  location: String
+  location: String,
+  image: String
 });
 
 const Don = mongoose.model("Don", donSchema);
